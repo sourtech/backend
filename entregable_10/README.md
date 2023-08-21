cfc72f6f48577c62e73a7cf14081513b933e6555
sourtech <hernanroig@gmail.com>
2023-08-20 21:52:08 -0300

    Entregable 10

---
+++ entregable_10/README.md
@@ -0,0 +1,13 @@
+## 📁 Documentar API
+
+### Consigna
+
+Realizar la configuración necesaria para tener documentado tu proyecto final a partir de Swagger.
+
+### Aspectos a incluir
+
+- Se debe tener documentado el módulo de productos.
+- Se debe tener documentado el módulo de carrito
+- No realizar documentación de sesiones
+
+### GET http://localhost:8080/apidocs/
---
+++ entregable_10/src/services/ProductsRepository.js
@@ -0,0 +1,32 @@
+
+export default class ProductRepository {
+
+    constructor(dao){
+        this.dao = dao;
+    }
+
+    getProducts = (options, req, paginate) => {
+        return this.dao.getProducts(options, req, paginate);
+    }
+
+    getProductById = (id) => {
+        return this.dao.getProductById(id)
+    }
+
+    addProduct = (product) => {
+        return this.dao.addProduct(product)
+    }
+
+    updateProduct = (id, product) => {
+        return this.dao.updateProduct(id,product)
+    }
+
+    deleteProduct = (id) => {
+        return this.dao.deleteProduct(id)
+    }
+/*
+    downStock = (id, product) => {
+         return this.dao.downStock(id, product)
+    }
+*/
+}
